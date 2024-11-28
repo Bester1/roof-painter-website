4e06e5e81a7f0a43914a40e721a97fa7bade00ec
Bester1 <abester7@gmail.com>
2024-11-28 16:39:47 +0200

    Updated form tracking to use standard GA4 event name

--- js/form-tracking.js
+++ js/form-tracking.js
@@ -2,19 +2,17 @@
 function handleFormSubmit(event) {
     event.preventDefault();
 
-    // Google Ads conversion tracking
-    gtag('event', 'conversion', {
-        'send_to': 'AW-11398569294/5cHACPyp9ZEYEO_Kzqsp'
+    // Google Analytics 4 event - using standard event name
+    gtag('event', 'generate_lead', {
+        'form_name': 'quote_request',
+        'form_destination': 'quote_submission',
+        'form_submit': 'success'
     });
 
-    // Google Analytics 4 event
-    gtag('event', 'form_submission', {
-        'event_category': 'Contact',
-        'event_label': 'Quote Request'
-    });
-
-    // Submit the form
-    event.target.submit();
+    // Submit the form after tracking
+    setTimeout(function() {
+        event.target.submit();
+    }, 500);
 
     return false;
 }
